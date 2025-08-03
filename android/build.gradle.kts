48b2c1ae50873495f714d2fb985feffa72f44f16
Kenzo0513 <95439162+Kenzo0513@users.noreply.github.com>
2025-08-03 10:58:46 -0500

    MSK-3: Configurar Firebase y Firestore

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,3 @@
-// 🔧 Sección necesaria para Firebase
 buildscript {
     repositories {
         google()
@@ -24,10 +23,6 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-
-// subprojects {
-//     project.evaluationDependsOn(":app")
-// }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
